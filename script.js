46f9ff5e06386a1dbb4c1ba77cba61871ba29ddc
Shio3001 <taku2000maru@icloud.com>
2024-02-20 23:34:35 +0900

    very happy

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const changePop = () => {
   const texth2 = document.querySelectorAll("h2");
   const texth3 = document.querySelectorAll("h3");
   const textp = document.querySelectorAll("p");
-
+  const happy = document.querySelector(".happy");
+  const project_sub_name_doc = document.getElementById("project_sub_name");
+  project_sub_name_doc.textContent = "プロジェクトページ 創英角ポップ体モード(隠し機能です。発見おめでとうございます！)";
   texth1.forEach((block) => {
     block.style.fontFamily = "HGS創英角ﾎﾟｯﾌﾟ体";
+    block.style.paddingTop = "10px";
   });
   texth2.forEach((block) => {
     block.style.fontFamily = "HGS創英角ﾎﾟｯﾌﾟ体";
@@ -15,5 +18,9 @@ const changePop = () => {
   });
   textp.forEach((block) => {
     block.style.fontFamily = "HGS創英角ﾎﾟｯﾌﾟ体";
+    block.style.backgroundColor = "#00ff00";
   });
+
+  happy.style.cursor = "wait";
+  happy.style.width = "100vw";
 };
